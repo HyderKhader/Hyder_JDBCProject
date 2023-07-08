67305be8fbf67d4eeed325449300622f9b1a75ed
HyderKhader <138754123+HyderKhader@users.noreply.github.com>
2023-07-08 22:21:43 +0530

    Add files via upload

---
+++ JDBC_SQL.sql
@@ -0,0 +1,5 @@
+create database Hyder;
+use Hyder;
+create table Login (username varchar(20),password varchar(20));
+create table Product (productId int,productName varchar(30),minSellQuantity int,price int,quantity int);
+insert into Login values("Hyder","786");
